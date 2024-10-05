4f86f072c7b6fd3db651b387e2a70c5076492fb0
Andrea-Coa <andrea.coa@utec.edu.pe>
2024-10-05 12:12:20 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Ingesta proyecto parcial Universidad
+
+## Contenidos:
+
+### General:
+
+- `crear_mv_ingesta.yaml`: Plantilla para crear máquina virtual de ingesta con CloudFormation.
+
+- `data_catalogs\`: contiene los data catalogs para AWS Glue
+
+### api-cursos
+
+- `ingesta.py`: obtener la data de la base de datos Mongo en MV Bases de datos y escribirla en archivos `.json`. Luego mandar los jsons a un bucket de S3.
+
+- `flatten_official.ipynb`: notebook para transformar los datos ingeridos a un modelo relacional, con el objetivo de realizar consultas. 
---
+++ ingesta.py
@@ -0,0 +1,60 @@
+from pymongo import MongoClient
+import json
+import boto3
+
+mongodb_url = "mongodb://172.31.47.47:27017/universidad"
+
+# todo esto estará dentro de un directorio output_data en la mv ingesta
+dir = "output_data/"
+files = ["cursos.json", "inscripcions.json", "ofertacursos.json"] 
+
+# bucket de S3 para guardar los datos ingeridos
+bucketname = "proyecto-parcial-out"
+
+def retrieve_data_from_collection_with_horarios(collection_name, outfile):
+    client = MongoClient(mongodb_url)
+    db = client.universidad    
+    collection = db[collection_name]
+
+    documents = collection.find()
+    with open(outfile, "w") as writefile:
+        for doc in documents:
+            # print(doc)
+            doc["_id"] = str(doc["_id"])
+            for horario in doc["horarios"]:
+                horario["_id"] = str(doc["_id"])
+            json.dump(doc, writefile)
+            writefile.write("\n")
+
+    print("Finished data retrieval from " + collection_name + " successfully.")
+
+def retrieve_data_from_cursos():
+    client = MongoClient(mongodb_url)
+    db = client.universidad    
+    collection = db.cursos
+    documents = collection.find()
+    with open(dir + files[0], "w") as writefile:
+        for doc in documents:
+            # print(doc)
+            doc["_id"] = str(doc["_id"])
+            json.dump(doc, writefile)
+            writefile.write("\n")
+    print("Finished data retrieval from cursos successfully.")
+
+def upload_to_s3(filenames, bucketname):
+    s3 = boto3.client('s3')
+    for filename in filenames:
+        response = s3.uploda_file(dir + filename, bucketname, filename)
+        print(response)
+    print("Ingesta completada")
+
+if __name__ == "__main__":
+    retrieve_data_from_cursos()
+    retrieve_data_from_collection_with_horarios("inscripcions", dir + files[1])
+    retrieve_data_from_collection_with_horarios("ofertacursos", dir + files[2])
+
+    upload_to_s3(files, bucketname)
+
+
+# RESOURCES
+# https://medium.com/@stevencibambo/loading-data-from-mongodb-with-pymongo-for-analysis-c0f61a8538a0
